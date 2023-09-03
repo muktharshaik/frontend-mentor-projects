f3c8b7bd34f814f39b43a6c66c717ba8f613bad3
mukthar <msshaikmukthar9@gmail.com>
2023-09-03 18:10:29 +0400

    init

---
+++ app/projects/newsletter/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './page.css';
+
+export default function Index() {
+  return (
+    <div className="page">
+      <h1 className="page">Newsletter</h1>
+    </div>
+  );
+}
